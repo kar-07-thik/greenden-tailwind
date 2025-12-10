36b1888fba9e237b25f54078a9fee343fc91f9b4
kar-07-thik <karthi533110@gmail.com>
2025-12-10 10:44:18 +0530

    update search functionality

--- product.js
+++ product.js
@@ -7,3 +7,20 @@ sideNavbarIcon.addEventListener("click", function () {
 menuclose.addEventListener("click", function () {
     sideNavbar.style.right = "-50%"
 })
+var search = document.getElementById("searchbar")
+var container = document.getElementById("productcontainer")
+var productList = productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup", function () {
+    var entervalue = event.target.value.toUpperCase()
+    for (count = 0; count<productList.length; count=count+1) {
+        var productname = productList[count].querySelector("h1").textContent
+        if (productname.toUpperCase().indexOf(entervalue) < 0) {
+            productList[count].style.display ="none"
+        }
+        else{
+            productList[count].style.display="block"
+        }
+    }
+
+})
